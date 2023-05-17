68ef8a8c1f2cad7244ac481b269c08bf22f194a6
Yuri Bulhões Bertoldi <ybulhoesbertoldi@gmail.com>
2023-05-17 18:57:56 -0300

    ajuste go mod

--- Main.go
+++ Main.go
@@ -3,23 +3,20 @@ package main
 import (
 	"fmt"
 
-	"github.com/YuriBertoldi/Go-Exemplo-ContaCorrente/Models/modelConta"
+	conta "github.com/YuriBertoldi/Go-ContaCorrente/Models"
 )
 
 func main() {
-	ccTitularTeste := new(modelConta.ContaCorrente)
-	ccTitularTeste.numeroAgencia = 123
-	ccTitularTeste.numeroConta = 5123123
-	ccTitularTeste.titular = "Titular teste 1"
+	fmt.Println("teste")
+	ccTitularTeste := new(conta.ContaCorrente)
+	ccTitularTeste.NumeroAgencia = 123
+	ccTitularTeste.NumeroConta = 5123123
+	ccTitularTeste.Titular = "Titular teste 1"
 	ccTitularTeste.Saldo = 102.45
 
-	// ccTitularTeste2 := ContaCorrente{titular: "Titular teste 2", numeroAgencia: 321, numeroConta: 123456, Saldo: 99.89}
-
-	ccTitularTeste1 := modelConta.ContaCorrente{"Titular teste 3", 321, 023456, 200}
+	ccTitularTeste1 := conta.ContaCorrente{"Titular teste 3", 321, 023456, 200}
 
 	fmt.Println(ccTitularTeste, " saque: ", ccTitularTeste.Sacar(100), " saldo disp: ", ccTitularTeste.Saldo)
-	// fmt.Println(ccTitularTeste2, " saque: ", ccTitularTeste2.sacar(100), " saldo disp: ", ccTitularTeste2.Saldo)
-	// fmt.Println(ccTitularTeste1, " Deposito: ", ccTitularTeste1.depositar(300), " saldo disp: ", ccTitularTeste1.Saldo)
 
 	fmt.Println(ccTitularTeste1, " Transferencia: ", ccTitularTeste1.Transferir(200, ccTitularTeste), " saldo disp: ", ccTitularTeste.Saldo)
 }
